0027806216febba9ccb6221bcf5d540158bbd4d2
Milad Doorbash <milad.doorbash@gmail.com>
2019-09-21 22:55:44 +0430

    tried to fix concurrency issue

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 const USERNAME string = "admin"
 const PASSWORD string = "1234"
+const DEBUG bool = true
 
 type LogData struct {
 	Type string
@@ -27,29 +28,41 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-var clientsChan chan *websocket.Conn = make(chan *websocket.Conn)
-var logsChan chan LogData = make(chan LogData)
+var newClientsChan chan chan LogData = make(chan chan LogData, 100)
+var removedClientsChan chan chan LogData = make(chan chan LogData, 100)
+var logsChan chan LogData = make(chan LogData, 100)
 
 func main() {
 	go func() {
-		// cmd := exec.Command("ping", "-t", "google.com")
-		cmd := exec.Command("pm2", "logs")
-		cmdReader, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-		scanner := bufio.NewScanner(cmdReader)
-		if err := cmd.Start(); err != nil {
-			log.Fatal(err)
-		}
-		for scanner.Scan() {
-			data := scanner.Text()
-			logsChan <- LogData{Type: "log", Data: data, Time: time.Now().UnixNano() / 1e6}
+		for {
+			if DEBUG {
+				fmt.Println("THREAD[1]: pm2 logs")
+			}
+			// cmd := exec.Command("ping", "-t", "google.com")
+			cmd := exec.Command("pm2", "logs")
+			cmdReader, err := cmd.StdoutPipe()
+			if err != nil {
+				log.Fatal(err)
+			}
+			scanner := bufio.NewScanner(cmdReader)
+			if err := cmd.Start(); err != nil {
+				log.Fatal(err)
+			}
+			for scanner.Scan() {
+				data := scanner.Text()
+				logsChan <- LogData{Type: "log", Data: data, Time: time.Now().UnixNano() / 1e6}
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Println(err)
+			}
 		}
 	}()
 
 	go func() {
 		for {
+			if DEBUG {
+				fmt.Println("THREAD[2]: pm2 jlist")
+			}
 			cmd := exec.Command("pm2", "jlist")
 			data, err := cmd.Output()
 			if err != nil {
@@ -62,42 +75,56 @@ func main() {
 	}()
 
 	go func() {
-		var clients map[*websocket.Conn]bool = make(map[*websocket.Conn]bool)
-		var clientsRemoveList []*websocket.Conn
+		var clients map[chan LogData]bool = make(map[chan LogData]bool)
 		for {
 			select {
-			case client := <-clientsChan:
+			case client := <-newClientsChan:
+				if DEBUG {
+					fmt.Println("added a new client")
+				}
 				clients[client] = true
+			case client := <-removedClientsChan:
+				if DEBUG {
+					fmt.Println("removed a client")
+				}
+				delete(clients, client)
 			case data := <-logsChan:
 				for client := range clients {
-					if err := client.WriteJSON(data); err != nil {
-						client.Close()
-						clientsRemoveList = append(clientsRemoveList, client)
-						continue
-					}
-				}
-				if len(clientsRemoveList) > 0 {
-					for _, clientToRemove := range clientsRemoveList {
-						delete(clients, clientToRemove)
-					}
-					clientsRemoveList = nil
+					client <- data
 				}
 			}
-
 		}
 	}()
 
 	http.Handle("/", httpauth.SimpleBasicAuth(USERNAME, PASSWORD)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("new http request: /")
 		http.ServeFile(w, r, "./index.html")
 	})))
 
 	http.Handle("/logs", httpauth.SimpleBasicAuth(USERNAME, PASSWORD)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if DEBUG {
+			fmt.Println("new http request: /logs")
+		}
 		var conn, err = upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		clientsChan <- conn
+		clientChan := make(chan LogData, 100)
+
+		newClientsChan <- clientChan
+
+		for {
+			select {
+			case data := <-clientChan:
+				if err := conn.WriteJSON(data); err != nil {
+					conn.Close()
+					removedClientsChan <- clientChan
+					return
+				}
+			}
+		}
+
 	})))
 
 	if err := http.ListenAndServe(":3030", nil); err != nil {
